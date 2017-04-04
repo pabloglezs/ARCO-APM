e46ed73b5ec72daf0a7a65c2a485b6fc2dd32583
agarca23 <agarca23@estudiantes.unileon.es>
2017-04-04 18:26:20 +0200

    Update mainwindow.h

--- Benchmark/mainwindow.h
+++ Benchmark/mainwindow.h
@@ -17,9 +17,15 @@ public:
     ~MainWindow();
 
     void algorithm1();
+    void algorithm2();
+
     double abrir();
+    double cargarImagen();
+
 
     int bandera = 0;
+    int bandera2 =0;
+
 
 private slots:
     void on_boton1_pressed();
@@ -27,6 +33,9 @@ private slots:
     void on_boton1_released();
 
     void on_boton1_clicked();
+    
+    void on_boton1_2_clicked();
+
 
 private:
     Ui::MainWindow *ui;
